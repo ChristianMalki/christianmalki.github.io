ed33ae877dcc96d62cca7838bfeef304da17d51f
Christian Malki <christianmalki00@hotmail.com>
2021-11-24 22:15:05 +0100

    Labbet
    Hello World

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Image, Button } from 'react-native';
 
 export default function App() {
-  return ( 
+
+
+    var textInputValue = "<Welcome to the dark side!>"; 
+    const handlePress = () => console.log(textInputValue); 
+    const handleChangeText = () => console.log(textInputValue);
+  
+    return (
     <ScrollView>
-      <View style={styles.container}>
-        <Text style={{FontWeight: "bold",fontSize: 50}}>Hello World!</Text>
-        <Text style={{ fontSize: 20}}>First React Native App</Text>
-        <Text>This is a paragraph explaning what this app is all about. As you can see there are already many components including text, images and buttonss</Text>
-        <StatusBar style="auto" />
-      </View>
-      
+      <View style={main.container}>
+      <TextInput placeholder="Initial Text" onChangeText={handleChangeText} />
+    <Button title="Submit" onPress={handlePress} />
+    <Text>Your answer is: {textInputValue}</Text>
+    </View>
+
       <View style={styles.container2}>
       <Text style={{ fontSize:20}}>What would you like to get out of this course?</Text>
       <TextInput placeholder="Initial text" />
@@ -21,8 +26,14 @@ export default function App() {
         source={require("./assets/icon.png")}
       />
       <StatusBar style="auto" />
+
+      <TextInput placeholder="Initial Text" onChangeText={handleChangeText} />
+    <Button title="Submit" onPress={handlePress} />
+    <Text>Your answer is: {textInputValue}</Text>
+
     </View>
   </ScrollView>
+
   );
 }
 
@@ -30,7 +41,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 0.3,
     backgroundColor: '#b6cfe4',
-   
+
     justifyContent: 'center',
   },
   contanier2: {
