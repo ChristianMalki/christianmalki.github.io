154ceb6a0dbf6f0fe6efd8e9d3eed7a36ff9d911
Christian Malki <christianmalki00@hotmail.com>
2021-11-24 14:20:16 +0100

    Hello
    Hello

--- main.js
+++ main.js
@@ -1,60 +1,77 @@
-function myTestFunction(test) {
+function awesomeFunction() {
+    // skapar en funktion som heter awesomeFunction
+    var firstName =prompt ("Please enter your name");
+    // Promt är ett meddelandefönster där man fyller i en textruta.
+    if (firstName != null) {
+        document.getElementById("demo").innerHTML= firstName + " is awesome!";
+        // getElementByID betyder att jag ska hämta ett element via  ID namnet.  
+    //  Har gjort en knapp på Home där man klickar på den för att kunna skriva sitt namnm
+    // Där det sedan kommer stå ditt namn och "is awesome":
+    }
+}
+
+// Automatic header slideshow
+var slideIndex = 0;  
+           //Declare index to 0
+        // Skapar en variabel som 
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("Picture");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    // De 3 bildern slider ett steg i taget
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
+}
+
+
+   
+    function displayDate() {
+        // Skapar en funktion att visa upp datum
+      document.getElementById("Tid").innerHTML = Date();
+    //   Du trycker på "The time is" knappen på Home så visar den dagens datum och tid
+    }
+ 
+    
 
 
 
-    console.log("Testet är: " + test);
+
+    try {
+        // Try testar att hitta fel i koden
+      Show("Pictures");
+    }
+    catch(error) {
+        // Catch hanterar genom att blockera koden 
     
-    
-    
+      console.error(" Oj detta var fel: " +  error);
+    //   Här skriver man ut felet som catch hittar
     }
     
-    
-    
-    
-    function myOtherFunction() {
-    
-    
-    
-    
-    document.getElementById("testet").innerHTML = "<p> Välkommen till min hemsida </p>";
-    
-    // null = ingenting ( inte ens noll )
-    
-    
-    
-    document.getElementById("my-element").setAttribute("id","my-new-id");
-    
-    
-    
-    
-    document.getElementById("my-new-id").setAttribute("style","background-color: red;");
-    
-    document.getElementById("my-new-id").style = "background-color: red";
-    
-    document.getElementById("my-new-id").setAttribute("class","cool-class");
-    
-    document.getElementById("my-new-id").innerHTML = "<!--Välkommen till min hemsida--> <p> Välkommen till min hemsida </p>";
-    
-    
-    
-    }
-    
-    
-    
-    document.getElementById("testet").addEventListener("click", function() {
-    
-    
-    
-    console.info("Du tryckte på TESTET!");
-    
-    
-    
-    });
-    
-    
-    
-    myOtherFunction();
-    
-    
-    
-    myTestFunction("Välkommen till min hemsida");
+
+
+
+
+   
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
